35d82f3986d9d34695fce7ca5dd0841ddda87d93
Ian Akotey <ianakotey@gmail.com>
2021-12-13 14:11:15 +0000

    Added config module for constants

---
+++ src/config.rs
@@ -0,0 +1,12 @@
+/// First row containin header data from CAMU
+/// Header row is currently row 9
+pub(crate) const ENROLLMENT_HEADER_ROW: usize = 9;
+
+/// Last column in CAMU detailed enrollment report
+/// Currently Column T which contains email data
+pub(crate) const ENROLLMENT_LAST_COLUMN: usize = 20;
+
+pub(crate) const ENROLLMENT_EMAIL_COL_INDEX: usize = 19;
+
+/// Index of column containing Student Id data. i.e. Column B
+pub(crate) const ENROLLMENT_STUD_ID_COL_INDEX: usize = 1;
