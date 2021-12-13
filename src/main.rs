f6f7d204ced10006389a5e0d18c2151797ca92c3
Ian Akotey <ianakotey@gmail.com>
2021-12-13 14:49:48 +0000

    Added enrollment module to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod cli;
+mod enrollment;
+mod config;
+mod utils;
 use cli::parse_args;
+use enrollment::parse_enrollment;
 
 fn main() {
     let args = parse_args();
+    let enrollment_data = parse_enrollment(& args.enrollment);
 }
