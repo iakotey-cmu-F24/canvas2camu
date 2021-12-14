1646de0c5e739646398977a553b662bc0beac306
Ian Akotey <ianakotey@gmail.com>
2021-12-14 12:43:12 +0000

    Added gradebook module to main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod cli;
-mod enrollment;
 mod config;
+mod enrollment;
+mod gradebook;
 mod utils;
 use cli::parse_args;
 use enrollment::parse_enrollment;
+use gradebook::parse_gradebook_file;
 
 fn main() {
     let args = parse_args();
-    let enrollment_data = parse_enrollment(& args.enrollment);
+    let enrollment_data = parse_enrollment(&args.enrollment);
+    let gradebook = parse_gradebook_file(&args.gradebook);
 }
