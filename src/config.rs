d411400887e694c2c625c57f9ccb0daf2dfdde6b
Ian Akotey <ianakotey@gmail.com>
2021-12-14 16:15:46 +0000

    Updated config info

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,21 @@
+use std::collections::HashMap;
+
 /// First row containin header data from CAMU
 /// Header row is currently row 9
 pub(crate) const ENROLLMENT_HEADER_ROW: usize = 9;
 
 /// Last column in CAMU detailed enrollment report
 /// Currently Column T which contains email data
-pub(crate) const ENROLLMENT_LAST_COLUMN: usize = 20;
-
 pub(crate) const ENROLLMENT_EMAIL_COL_INDEX: usize = 19;
 
 /// Index of column containing Student Id data. i.e. Column B
 pub(crate) const ENROLLMENT_STUD_ID_COL_INDEX: usize = 1;
 
-pub(crate) const GRADEBOOK_USELESS_COL_COUNT: usize = 2;
-pub(crate) const GRADEBOOK_USELESS_LINE_COUNT: usize = 2;
-
 pub(crate) const GRADEBOOK_NON_GRADE_COL_COUNT: usize = 4;
 
 pub(crate) const GRADEBOOK_CSV_DELIMITER: char = ',';
+
 pub(crate) const GRADEBOOK_EMAIL_COL_INDEX: usize = 3;
+
+
+pub(crate) type Gradebook = HashMap<String, (usize, HashMap<String, String>)>;
