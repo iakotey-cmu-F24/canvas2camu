64bd9018dad54ad86aca15dcd99d4d855fb69a01
Ian Akotey <ianakotey@gmail.com>
2021-12-22 20:12:59 +0000

    Added writer module to main

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod config;
 mod enrollment;
 mod gradebook;
 mod utils;
+mod writer;
 use cli::parse_args;
 use enrollment::parse_enrollment;
 use gradebook::parse_gradebook_file;
-
+use writer::create_file;
 fn main() {
     let args = parse_args();
     let enrollment_data = parse_enrollment(&args.enrollment);
