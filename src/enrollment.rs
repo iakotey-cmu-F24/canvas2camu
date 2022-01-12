6e2ec6321a70837714d6e2569d17cf2c6ef178c6
Ian Akotey <ianakotey@gmail.com>
2022-01-12 11:20:01 +0000

    Refactor enrollment module to return new enrollment type

--- src/enrollment.rs
+++ src/enrollment.rs
@@ -1,27 +1,30 @@
 use crate::config;
 use crate::utils;
 use office::{DataType, Excel};
-use std::collections::HashMap;
 
 pub(crate) fn parse_enrollment(filename: &str) -> config::EnrollmentData {
-    let mut workbook = Excel::open(filename).expect(&format!("Unable to open enrollment workbook: {}", filename));
-    let mut data = HashMap::new();
+    let mut workbook = Excel::open(filename)
+        .expect(&format!("Unable to open enrollment workbook: {}", filename));
 
-    if let Ok(range) = workbook.worksheet_range("Detailed Enrollment List") {
-
-        for row in range.rows().skip(config::ENROLLMENT_HEADER_ROW) {
-            data.insert(
+    workbook
+        .worksheet_range(config::ENROLLMENT_SHEET_NAME).unwrap()
+        .rows()
+        .skip(config::ENROLLMENT_HEADER_ROW)
+        .map(|row| {
+            (
                 utils::cast!(
                     row[config::ENROLLMENT_EMAIL_COL_INDEX].to_owned(),
+                    DataType::String
+                ),
+                utils::cast!(
+                    row[config::ENROLLMENT_STUD_NAME_COL_INDEX].to_owned(),
                     DataType::String
                 ),
                 utils::cast!(
                     row[config::ENROLLMENT_STUD_ID_COL_INDEX].to_owned(),
                     DataType::String
                 ),
-            );
-        }
-    }
+            )
+        }).collect::<config::EnrollmentData>()
 
-    data
 }
