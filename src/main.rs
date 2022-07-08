a6729cbeffa8d1154b557b33318edd6fe0f1973a
Ian Akotey <ian.akotey@gmail.com>
2022-07-08 10:30:44 +0000

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//! `Grade_Gen` is a rust program to generate CAMU bulk-upload-compatible
+//! excel files from Canvas' gradebook.
+
+
+
 mod cli;
 mod config;
 mod enrollment;
@@ -7,9 +12,14 @@ mod writer;
 use cli::parse_args;
 use enrollment::parse_enrollment;
 use gradebook::parse_gradebook_file;
-use writer::create_file;
+use writer::create_files;
 fn main() {
     let args = parse_args();
     let enrollment_data = parse_enrollment(&args.enrollment);
     let gradebook = parse_gradebook_file(&args.gradebook);
+    // println!("{:#?}", enrollment_data);
+    // println!("{:#?}", gradebook);
+    // println!("{:#?}", args);
+    // create_files(r"D:\OneDrive\Projects\grade_gen\src\tests\Assets\tmp", &gradebook, &enrollment_data);
+    create_files(&args.output_dir.to_str().unwrap(), &gradebook, &enrollment_data);
 }
