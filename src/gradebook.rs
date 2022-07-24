dcf0b3c4cc4c5b0e3c18c316207b972787d815dd
Ian Akotey <ianakotey@gmail.com>
2022-07-24 01:06:25 +0100

    Added error handling to data insertion andimporved email detection algo

--- src/gradebook.rs
+++ src/gradebook.rs
@@ -1,6 +1,7 @@
 use crate::config;
 use std::collections::HashMap;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
@@ -20,6 +21,11 @@ pub enum GradebookError {
 
     #[snafu(display("Unable to parse entry #{index}"))]
     EntryParseError { index: usize, source: csv::Error },
+
+    #[snafu(display(
+        "Unable to retrieve field {field_index} of entry #{entry_index}"
+    ))]
+    EntryFieldNotFoundError { entry_index: usize, field_index: usize },
 
     #[snafu(display("Required header 'SIS Login ID' in gradebook"))]
     EmailHeaderNotFoundError,
@@ -44,6 +50,13 @@ pub(crate) fn parse_gradebook_file(
     let mut headers =
         records.next().context(FileEmptySnafu)?.context(HeaderParseSnafu)?;
 
+    let header_email_index = headers
+        .iter()
+        .enumerate()
+        .filter(|x| x.1.eq_ignore_ascii_case("SIS Login ID"))
+        .next()
+        .context(EmailHeaderNotFoundSnafu)?
+        .0;
 
     let mut points_row = loop {
         let record =
@@ -62,7 +75,6 @@ pub(crate) fn parse_gradebook_file(
         .iter()
         .zip(points_row.iter())
         .enumerate()
-        .skip(config::GRADEBOOK_NON_GRADE_COL_COUNT)
         .map(|(idx, (course, points))| (idx, course.trim(), points))
         .filter(|(_, _, points)| {
             !points.eq_ignore_ascii_case("Points Possible")
@@ -76,20 +88,28 @@ pub(crate) fn parse_gradebook_file(
         course_grades.insert(title.trim().to_string(), (index, HashMap::new()));
     });
 
-    records.for_each(|record| match record {
-        Err(why) => panic!("{}", why),
-        Ok(line) => {
-            course_grades.iter_mut().for_each(|(_, (index, map))| {
-                map.insert(
-                    line.get(config::GRADEBOOK_EMAIL_COL_INDEX)
-                        .unwrap()
-                        .trim()
-                        .to_string(),
-                    line.get(*index).unwrap().trim().to_string(),
-                );
-            });
-        },
-    });
+    for (idx, record) in records.enumerate() {
+        let mut record = record.context(EntryParseSnafu { index: idx + 1 })?;
+        record.trim();
+        for (_, (index, map)) in course_grades.iter_mut() {
+            map.insert(
+                record
+                    .get(header_email_index)
+                    .context(EntryFieldNotFoundSnafu {
+                        entry_index: idx + 1,
+                        field_index: header_email_index,
+                    })?
+                    .to_string(),
+                record
+                    .get(*index)
+                    .context(EntryFieldNotFoundSnafu {
+                        entry_index: idx + 1,
+                        field_index: *index,
+                    })?
+                    .to_string(),
+            );
+        }
+    }
 
     Ok(course_grades)
 }
